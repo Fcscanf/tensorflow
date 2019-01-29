1fcf8d01847cde8d2d48545b52fa181228487666
Fcscanf <fcscanf@gmail.com>
2019-01-30 00:58:30 +0800

    Tensorflow的常用API和变量作用域

--- com/fcant/tensorflow/owt/p1/02compilemodel.py
+++ com/fcant/tensorflow/owt/p1/02compilemodel.py
@@ -16,17 +16,24 @@ matrx1 = tf.constant([[3., 3.]])
 matrx2 = tf.constant([[2.], [2.]])
 
 """
-变量
+变量-创建一个变量，初始化标量0
 """
 state = tf.Variable(0, name="counter")
 
 """
 创建一个常量张量
 """
+#input1 = tf.constant(3.0)
 input1 = tf.placeholder(tf.float32)
 input2 = tf.placeholder(tf.float32)
 
+"""
+创建一个矩阵乘法，把matrix1和matrix2作为输入
+"""
 c = a * b
+"""
+返回值product代表矩阵乘法的结果
+"""
 product = tf.matmul(matrx1, matrx2)
 output = tf.multiply(input1, input2)
 
@@ -34,7 +41,10 @@ output = tf.multiply(input1, input2)
 创建会话
 """
 with tf.Session() as sess:
+    #指在第二个GPU上运行
+    #with tf.device("/gpu:1"):
     print(sess.run(c))
     print(sess.run([product]))
     print(sess.run([output], feed_dict={input1:[7.], input2:[2.]}))
+    #输出[array([14.], dtype=float32)]
     sess.close()
--- com/fcant/tensorflow/owt/p1/04variable_scope.py
+++ com/fcant/tensorflow/owt/p1/04variable_scope.py
@@ -12,3 +12,66 @@ import tensorflow as tf
 为变量指明命名空间
 """
 #tf.variable_scope(<scope_name>)
+t = tf.add(8, 9)
+print(t)   #输出Tensor("Add:0", shape=(), dtype=int32)
+
+# with tf.variable_scope("foo"):
+#     v = tf.get_variable("v", [1])
+#     v2 = tf.get_variable("v", [1])
+# assert v.name == "foo/v:0"
+
+with tf.variable_scope("foo") as scope:
+    v = tf.get_variable("v", [1])
+with tf.variable_scope("foo", reuse=True):
+    #也可以写成
+    # scope.reuse_vaiables()
+    v1 = tf.get_variable("v", [1])
+assert v1 == v
+
+"""
+获取变量作用域
+"""
+with tf.variable_scope("foo") as foo_scope:
+    v = tf.get_variable("v", [1])
+with tf.variable_scope(foo_scope):
+    w = tf.get_variable("w", [1])
+
+
+with tf.variable("foo") as foo_scope:
+    assert foo_scope.name == "foo"
+with tf.variable_scope("bar"):
+    with tf.variable_scope("baz") as other_scope:
+        assert other_scope.name == "bar/baz"
+        with tf.variable_scope(foo_scope) as foo_scope2:
+            assert foo_scope2.name == "foo" #保持不变
+
+"""
+变量作用域的初始化
+"""
+with tf.variable_scope("foo", initializer=tf.constant_initializer(0.4)):
+    v = tf.get_variable("v", [1])
+    assert v.eval() == 0.4 #被作用域初始化
+    w = tf.get_variable("w", [1], initializer=tf.constant_initializer(0.3))
+    assert w.eval() == 0.3 #重写初始化的值
+    with tf.variable_scope("bar"):
+        v = tf.get_variable("v", [1])
+        assert v.eval() == 0.4 #继承默认的初始化器
+    with tf.variable_scope("baz", initializer=tf.constant_initializer(0.2)):
+        v = tf.get_variable("v", [1])
+        assert v.eval() == 0.2 #重写父作用域的初始化器的值
+
+with tf.variable_scope("foo"):
+    x = 1.0 + tf.get_variable("v", [1])
+    assert x.op.name == "foo/add"
+
+"""
+name_scope操作示例
+"""
+with tf.variable_scope("foo"):
+    with tf.name_scope("bar"):
+        v = tf.get_variable("v", [1])
+        b = tf.Variable(tf.zeros([1], name='b'))
+        x = 1.0 + v
+assert v.name == "foo/v:0"
+assert b.name == "foo/bar/b:0"
+assert x.op.name == "foo/bar/add"

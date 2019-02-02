a59ed5a1da623995965212d7f24def0f7b5909e9
Fcscanf <fcscanf@gmail.com>
2019-02-02 20:30:39 +0800

    Tensorflow-优化方法

---
+++ com/fcant/tensorflow/owt/p2_Neuronfunction/Optimizationfunction.py
@@ -0,0 +1,21 @@
+"""
+@author:Fcant
+优化方法
+"""
+
+import tensorflow as tf
+import numpy as np
+
+"""
+class tf.train.GradientDescentOptimizer
+class tf.train.AdadeltaOptimizer
+class tf.train.AdagradOptimizer
+class tf.train.AdagradDAOptimizer
+class tf.train.MomentumOptimizer
+class tf.train.AdamOptimizer
+class tf.train.FtrlOptimizer
+class tf.train.RMSPropOptimizer
+"""
+
+
+
